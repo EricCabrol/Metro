8d70347e5804ec1632f24272b3fe25a30f4f0d8b
Eric Cabrol <eric.cabrol@gmail.com>
2024-05-06 13:42:07 +0200

    sign inversion if Yinv found in the recording's name

--- find_jolts_all.py
+++ find_jolts_all.py
@@ -8,24 +8,33 @@ import os
 import re
 from shutil import copy
 
-# data_folder = Path('./data')
-data_folder = Path('./data_test')
-
+data_folder = Path('./data')
+# data_folder = Path('./data_test')
 subfolders = [ f.path for f in os.scandir(data_folder) if f.is_dir() ]
 
-fs_calibrated = 50 # sampling frequency
-min_amplitude_jolt = 1.5 # minimum acceleration to consider the peak as a jolt (m/s^2)
-min_amplitude_restart = 0.5 # minimum acceleration to consider that the metro restarts (m/s^2)
-min_stop_duration = 20 # min duration before peak and restart (s)
-nok_trips = []
+# INITIALISATION
+
+fs_calibrated = 50 # (Hz) sampling frequency
+min_amplitude_jolt = 1.5 # (m/s^2) minimum acceleration to consider the peak as a jolt 
+min_amplitude_restart = 0.5 # (m/s^2) minimum acceleration to consider that the metro restarts 
+min_stop_duration = 20 # (s) min duration before peak and restart 
+nok_trips = [] # list of trips for which timestamps will require a manual validation
+
+# LOOP ON TRIPS
 
 for trip in subfolders:
-
-    print(trip)
-    # TODO : continue loop when timestamps_valid.txt already exists
-    # TODO 2 : modify accel sign when Yinv
+    trip_name = Path(trip).stem
+    print(trip_name)
+    # if the timestamps have already been validated skip to next trip
+    if os.path.exists(Path(trip) / "timestamps_valid.txt"):
+        continue
+    # TODO : modify accel sign when Yinv
     df = pd.read_csv(Path(trip) / 'Accelerometer.csv')
     df['time'] = df['time']-df['time'][0] # Reset initial time
+
+    # Invert the sign of Y accel if the trip name requires it
+    if re.search("Yinv",trip_name):
+        df['y'] = -1*df['y']
 
     begin = 0 # init
     end = 0 # index init
@@ -56,7 +65,7 @@ for trip in subfolders:
     fig.add_trace(go.Scatter(x = df['time']/1e9, y = df['stop'], name = 'stop')) 
 
     fig.update_layout(
-        title="Calibrated acceleration and stops for trip "+trip,
+        title="Calibrated acceleration and stops for trip "+trip_name,
         xaxis_title="time (s)",
         yaxis_title="acceleration (m/s^2)",
         yaxis_range=[-4,4]
@@ -66,12 +75,12 @@ for trip in subfolders:
 
     # ASK USER INPUT FOR CONFIRMATION
 
-    valid = input("Are the timestamps OK ? (y/n)")
+    valid = input("Are the timestamps OK ? (y/n) ")
 
     if re.match("[yY]",valid):
         copy(Path(trip) / "timestamps.txt",Path(trip) / "timestamps_valid.txt")
     else:
-        nok_trips.append(Path(trip).stem)
+        nok_trips.append(trip_name)
 
 
 print("List of NOK trips")
