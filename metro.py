12bf9e8e5a2b3dcf46531510b1ed38457036f6f3
EricCabrol <eric.cabrol@gmail.com>
2024-05-16 23:23:29 +0200

    added sequence matcher

--- metro.py
+++ metro.py
@@ -1,5 +1,6 @@
 import re
 import unidecode
+from difflib import SequenceMatcher
 
 class Trip:
 
@@ -49,26 +50,36 @@ class Trip:
             trip_start = self.get_start()
             trip_end = self.get_end()
             trip_line = self.get_line()
-            line_stations = stations[trip_line]
+            line_stations = stations[trip_line] # only to improve readability 
+            # in case of mistyping, replace by a close enough match
+            if (trip_start not in line_stations):
+                print('First station identified from regexp : ',trip_start)
+                max_match = 0
+                for station in line_stations:
+                    s = SequenceMatcher(None,trip_start, station)
+                    if s.ratio() > max_match:
+                        kept = station
+                        max_match = s.ratio()
+                trip_start = kept
+                print('First station is likely to be : ', trip_start)
+                if max_match < 0.8 : # arbitrary threshold, works quite well
+                    print ('Warning ! Confidence threshold too low : ', max_match)
             # if the trip is in the same direction as the line definition
             if (line_stations.index(trip_start) < line_stations.index(trip_end)):
                 # return the corresponding slice of the line stations, including the end
-                return(line_stations[line_stations.index(trip_start):line_stations.index(trip_end)+1]) # add 1 to get the last station
-            else:
-                # do the opposite :)
+                # (add 1 to get the last station)
+                return(line_stations[line_stations.index(trip_start):line_stations.index(trip_end)+1]) 
+            else:  # do the opposite :)
                 tmp = line_stations[line_stations.index(trip_start):line_stations.index(trip_end):-1]
-                tmp.append(line_stations[line_stations.index(trip_end)]) # ugly hack to get the first station (couldn't find a better way)
+                # ugly hack to get the first station (couldn't find a better way)
+                tmp.append(line_stations[line_stations.index(trip_end)]) 
                 return(tmp)
         except:
             print("Could not retrieve stops for "+self.name)
 
-
-
-    
-
 if __name__=='__main__':
 
-    trip_list = ['L3_Sentier_-_St-Lazare_-2024-05-03_17-04-03']
+    trip_list = ['L3_Sentir_-_St-Lazare_-2024-05-03_17-04-03']
     for t in trip_list:
         print(t)
         trip = Trip(t)
