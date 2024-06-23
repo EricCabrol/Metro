64887c07bbcf6beca2b90416d720b4a8d1bb4254
EricCabrol <eric.cabrol@gmail.com>
2024-06-23 11:12:05 +0200

    separate window size from min stopped time => OK

--- compute_sliding_window_stats.py
+++ compute_sliding_window_stats.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
-
+import metro 
 
 
 
@@ -23,23 +23,46 @@ def compute_sliding_window_stats(df, column_name, window_size):
 
     return means, variances
 
-# Example usage
-csv_file = 'data/L4_Montparnasse_-_Reaumur_-_bag-2024-01-25_12-16-45/Accelerometer.csv'
+def identify_time_windows(signal, threshold, min_window_length):
+
+    
+    below_threshold = signal < threshold
+    below_threshold = below_threshold.astype(int)
+    
+    # Identify the start and end of each window
+    diff = np.diff(below_threshold, prepend=0, append=0)
+    starts = np.where(diff == 1)[0]
+    ends = np.where(diff == -1)[0]
+    
+    # Filter out windows that are shorter than the minimum length
+    valid_windows = [(start, end-1) for start, end in zip(starts, ends) if (end - start) >= min_window_length]
+
+    result_vector = np.zeros_like(signal, dtype=int)
+    for start, end in valid_windows:
+        result_vector[start:end+1] = 1
+
+    return valid_windows, result_vector
+
+# Application
+csv_file = 'data/L4_Montparnasse_-_Reaumur_-_bag-2024-01-25_12-16-45/AccelerometerUncalibrated.csv'
 column_name = 'y'
-window_size = 100
+record = metro.Record(csv_file)
+sampling_frequency = record.get_frequency()
+print("Sampling frequency = ",sampling_frequency)
+window_size = int(2 * sampling_frequency)
+min_stopped_time = int(10 * sampling_frequency)
 df = pd.read_csv(csv_file)
 
 means, variances = compute_sliding_window_stats(df, column_name, window_size)
 
-# Print the results
-# print("Means:", means)
-# print("Variances:", variances)
+windows, stopped = identify_time_windows(np.array(variances), 0.01, min_stopped_time)
 
 
 
 # Plot the time evolution of the variance
 plt.figure(figsize=(10, 6))
-plt.plot(range(len(variances)), variances, label='Variance')
+# plt.plot(range(len(variances)), variances, label='Variance')
+plt.plot(range(len(variances)), stopped, label='Stopped')
 plt.plot(df.index, df[column_name], label='Accel')
 plt.xlabel('Time')
 plt.ylabel('Variance')
